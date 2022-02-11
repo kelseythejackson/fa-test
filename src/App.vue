12ef8c5fa7f1555a9ca73f1c63cff4a6e8574cf9
Kelsey Jackson <kelseythej@gmail.com>
2022-02-11 12:19:01 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    FA Icons
+    <font-awesome-icon icon="fa-solid fa-user-secret" />
+    <font-awesome-icon icon="user-secret" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
